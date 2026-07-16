b85a78a3de2ca418be329ab04a038dc18898d3fa
Thomas Petit-Jean <thomas.petitjean@ipsosante.fr>
2026-07-16 12:20:04 +0200

    use insta also

---
+++ run_combined.sh
@@ -0,0 +1,21 @@
+uv run python -m yt_dlp \
+  --flat-playlist \
+  --print webpage_url \
+  "http://www.youtube.com/playlist?list=PLum-UT-EsfCWXirYrz4y-RI972xf8i-Eu" \
+  > sources.txt
+
+cat instagram_saved.txt >> sources.txt
+
+uv run python -m yt_dlp --js-runtimes node \
+  --verbose \
+  --cookies-from-browser chrome \
+  --download-archive downloaded.txt \
+  --batch-file sources.txt \
+  --ignore-errors \
+  --extract-audio \
+  --audio-format mp3 \
+  --audio-quality 0 \
+  --embed-metadata \
+  -o "data/%(autonumber)04d.%(ext)s"
+
+ls -1 data/*.mp3 2>/dev/null | wc -l | tr -d ' ' > number.txt
